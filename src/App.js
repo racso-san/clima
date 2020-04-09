912b4507a83ff133a5fd363425f008ba17413328
oscar <oll301189@gmail.com>
2020-04-09 17:11:32 -0300

    Form,app,components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React,{Fragment} from 'react';
+import React,{Fragment,useState,useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [busqueda,guardarBusqueda] = useState({
+    ciudad:'',
+    pais:''
+  });
+
+  const [consultar,guardarConsultar] = useState(false);
+
+  const {ciudad,pais} = busqueda;
+
+  useEffect (() => {
+    const consultarAPI = async () => {
+    
+      if(consultar){ // Solo muestra si consulta es true
+        const appId = '0107175d88b41900705d98cb99caa9cc';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        
+        console.log(resultado);
+      }  
+
+    }
+    consultarAPI();
+  },[consultar]); // Cuando "consultar" pasa a "true" , hace la consulta 
+
   return (
     <Fragment>
       <Header
@@ -12,7 +39,11 @@ function App() {
         <div className="container">
           <div className0="row">
             <div className="col m5 s12">
-              <Formulario/>
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
             </div>
             <div className="col m6 s12">
               2
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-const Formulario = () => {
+const Formulario = ({busqueda,guardarBusqueda,guardarConsultar}) => {
+
+
+    const [error,guardarError] = useState(false);
+
+    const {ciudad,pais} = busqueda;
+
+    // Funcion que coloca los elementos en el state
+    const handleChange = (e) => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Validar
+        if(ciudad.trim()==='' || pais.trim()===''){
+            guardarError(true);
+            return;
+        };
+
+        guardarError(false);
+
+        guardarConsultar(true);
+
+    };
+
     return ( 
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <p className="red darken-4 error">Todos los campos son obligatorios</p> : null}
 
             <div className="input-field col s12">
                 <input
                     type="text"
                     name="ciudad"
                     id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
                 />
                 <label htmlFor="ciudad">Ciudad</label>
             </div>
@@ -17,10 +51,27 @@ const Formulario = () => {
                 <select
                     name="pais"
                     id="pais"
+                    value={pais}
+                    onChange={handleChange}
                 >
-                    <option value="">--Seleccione un pais--</option>    
+                    <option value="">--Seleccione un pais--</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>    
                 </select>
                 <label htmlFor="pais">Pais:</label>
+            </div>
+
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    value="Buscar Clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
 
         </form>
